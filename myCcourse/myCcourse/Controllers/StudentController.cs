918392ba436cd3cd683e9ee34968c23815c5763a
汪坤 <7606511+wangkunbaba@user.noreply.gitee.com>
2021-05-28 01:47:26 +0800

    第二次更新

--- myCcourse/myCcourse/Controllers/StudentController.cs
+++ myCcourse/myCcourse/Controllers/StudentController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using myCcourse.Models;
 
 namespace myCcourse.Controllers
 {
     public class StudentController : Controller
     {
+        CourseEntities4 db = new CourseEntities4();
         // GET: Student
         public ActionResult Index()
         {
+            string name= Session["s_name"].ToString();
+            View_student_class_college s = db.View_student_class_college.SingleOrDefault(t=>t.s_name.Contains(name));
+            ViewBag.studentName = s.s_name;
+            ViewBag.classNum = s.classNum;
+            ViewBag.collegeName = s.collegeName;
             return View();
         }
     }
--- myCcourse/myCcourse/Controllers/TeacherController.cs
+++ myCcourse/myCcourse/Controllers/TeacherController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using myCcourse.Models;
 
 namespace myCcourse.Controllers
 {
     public class TeacherController : Controller
     {
+        CourseEntities4 db = new CourseEntities4();
         // GET: Teacher
         public ActionResult Index()
         {
+            string name = Session["t_name"].ToString();
+            View_teacer_Type t = db.View_teacer_Type.SingleOrDefault(a => a.t_name == name);
+            ViewBag.teacherName = t.t_name;
+            ViewBag.type = t.type;
             return View();
         }
     }
