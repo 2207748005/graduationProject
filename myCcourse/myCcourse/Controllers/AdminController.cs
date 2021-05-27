9ef3580419b3ef99829b621cee966ed662b51a6d
汪坤 <7606511+wangkunbaba@user.noreply.gitee.com>
2021-05-27 23:07:12 +0800

    第一次提交

---
+++ myCcourse/myCcourse/Controllers/AdminController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using myCcourse.Models;
+
+namespace myCcourse.Controllers
+{
+    public class AdminController : Controller
+    {
+        CourseEntities4 db = new CourseEntities4();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            List<View_teacer_Type> list = db.View_teacer_Type.ToList();
+            ViewData.Model = list;
+            return View("");
+        }
+        public ActionResult AllSelsect()
+        {
+
+            List<View_student_class_college> list = db.View_student_class_college.ToList();
+
+            return PartialView("All", list);
+        }
+        public ActionResult All() 
+        {
+            return View("");
+        }
+        public ActionResult Teacher() 
+        {
+            return View();
+        }
+
+        public ActionResult TeacherSelect() 
+        {
+            List<View_teacer_Type> list = db.View_teacer_Type.ToList();
+            return PartialView("Teacher", list);
+        }
+
+        public ActionResult studentTable() 
+        {
+            //List<Viewt_class_college> list = db.Viewt_class_college.ToList();
+            return View("");
+        }
+
+        public ActionResult Student()
+        {
+            //查询视图
+            List<Viewt_class_college> list = db.Viewt_class_college.ToList();
+            return PartialView("studentTable", list);
+        }
+
+        public ActionResult collegeTable() 
+        {
+            return View("");
+        }
+        public ActionResult College() 
+        {
+            List<college> list = db.colleges.ToList();
+            return PartialView("collegeTable", list);
+        }
+    }
+}
---
+++ myCcourse/myCcourse/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using myCcourse.Models;
+
+namespace myCcourse.Controllers
+{
+    public class HomeController : Controller
+    {
+        //实例化模型类
+        CourseEntities4 db = new CourseEntities4();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        // Post: Home
+        //管理员登录方法
+        public ActionResult adminLogin(string username,string password) 
+        {
+            Admin a = db.Admins.SingleOrDefault(t => t.name == username);
+            if (a!=null)
+            {
+                if (a.password == password)
+                {
+                    //把用户存到session里面
+                    Session["LoginUserName"] = a.name;
+                    return RedirectToAction("index", "Admin");
+                }
+                else {
+                    ViewBag.Error = "密码错误！";
+                    return View("Login");
+                }
+            }
+            else
+            {
+                ViewBag.Error = "用户名错误";
+                return View("Login");
+            }
+            
+        }
+        //学生验证登录
+        public ActionResult studentLogin(string username, string password)
+        {
+            student a = db.students.SingleOrDefault(t => t.s_name==username);
+            if (a != null)
+            {
+                if (a.s_password == password)
+                {
+                    //把用户存到session里面
+                    Session["s_name"] = a.s_name;
+                    return RedirectToAction("index", "Student");
+                }
+                else
+                {
+                    ViewBag.Error2 = "密码错误！";
+                    return View("Login");
+                }
+            }
+            else
+            {
+                ViewBag.Error2 = "用户名错误";
+                return View("Login");
+            }
+            
+        }
+        //验证教师登录
+        public ActionResult teacherLogin(string username,string password)
+        {
+            teacher a = db.teachers.SingleOrDefault(t => t.t_name == username);
+            if (a != null)
+            {
+                if (a.t_password == password)
+                {
+                    //把用户存到session里面
+                    Session["t_name"] = a.t_name;
+                    return RedirectToAction("index", "Teacher");
+                }
+                else
+                {
+                    ViewBag.Error1 = "密码错误！";
+                    return View("Login");
+                }
+            }
+            else
+            {
+                ViewBag.Error1 = "用户名错误";
+                return View("Login");
+            }
+            
+        }
+        public ActionResult index() 
+        {
+            return View();
+        }
+    }
+}
